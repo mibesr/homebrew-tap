efef1c0d1b5f98bc692e7dab7d4380eed158ca55
Lean <lean@Leans-MacBook-Pro.local>
2026-09-13 17:45:08 +0800

    add echomusic

---
+++ Casks/echomusic.rb
@@ -0,0 +1,27 @@
+cask "echomusic" do
+  version "2.3.2-beta.3"
+  sha256 :no_check
+
+  url "https://github.com/hoowhoami/EchoMusic/releases/download/v2.3.2-beta.3/EchoMusic-#{version}-macOS-#{arch}.dmg",
+      verified: "https://github.com"
+
+  name "EchoMusic"
+  desc "音乐播放器"
+  homepage "https://github.com/hoowhoami/EchoMusic"
+
+  on_intel do
+    arch = "x64"
+  end
+  on_arm do
+    arch = "arm64"
+  end
+
+  app "EchoMusic.app"
+
+  zap trash: [
+    "~/Library/Application Support/EchoMusic",
+    "~/Library/Logs/EchoMusic",
+    "~/Library/Preferences/cc.cd.echomusic.plist",
+    "~/Library/Saved Application State/cc.cd.echomusic.savedState",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,10 @@
+Homebrew Tap
+-------------------
+
+Install the Application with
+
+```bash
+brew install --cask mibesr/echomusic
+```
+
+For more information, visit https://github.com/hoowhoami/EchoMusic
